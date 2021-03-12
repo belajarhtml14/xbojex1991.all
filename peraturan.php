37aff468681eec6ae01ce7effdf5615e832ee594
belajarhtml14 <80535452+belajarhtml14@users.noreply.github.com>
2021-03-13 00:31:46 +0700

    Create peraturan.php

---
+++ peraturan.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Instagram Followers">
+    <meta name="author" content="MasterOgar RENDY">
+    <link href="assets/images/favicon/favicon.png" rel="icon" type="image/x-icon" />
+    <title>Instagram Followers</title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/main-style.css" rel="stylesheet">
+    <link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <style>
+        body {
+            padding-top: 100px;
+            background-image: url("assets/images/slide_01.jpg");
+        }
+    </style>
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body class="backcuk">
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+         <a href="index.php" class="navbar-brand">Instagram Followers</a>        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="Dashboard" onclick="this.href = 'index.php'"><i class="glyphicon glyphicon-home"></i> Dashboard</a></li>
+            <li class="active"><a href="Peraturan" onclick="this.href = 'peraturan.php'"><i class="fa fa-info-circle"></i> Peraturan</a></li>
+            <li><a href="Hubungi kami" onclick="this.href = 'kontak.php'"><i class="glyphicon glyphicon-earphone"></i> Hubungi kami</a></li>
+            <li><a href="Masuk Instagram" onclick="this.href = 'masuk.php'"><i class="glyphicon glyphicon-off"></i> Masuk Instagram</a></li>          </ul>
+          </div>
+      </div>
+    </nav>
+    <div class="container">
+        <div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+            <div class="panel panel-info" >
+                    <div class="panel-heading">
+                        <div class="panel-title">Peraturan dan Penggunaan Layanan</div>
+                    </div> 
+                    <div class="panel-body">
+                        <h2 class="text-center">PERATURAN</h2><hr>
+                                        <ol>
+                                            <li>
+                                                Jangan mengunfollow sesama pengguna layanan ini.Cukup mention mereka untuk follback
+                                            </li>
+                                            <li>
+                                                Jangan mem-Private akun anda,jika anda mem-Private akun anda,followers/like tidak akan bertambah.
+                                            </li>
+                                            <li>
+                                                Layanan ini gratis,mohon gunakan layanan ini dengan bijak
+                                            </li>
+                                            <li>
+                                                Use this tools at your own risk
+                                            </li>
+                                            <li>
+                                                Ingat kami tidak pernah menyimpan password anda!,Kami hanya memerlukan password anda pada saat login untuk menghubungkan akun anda dengan API Instagram.
+                                            </li>
+                                        </ol><hr>
+                        <h2 class="text-center">BANTUAN</h2><hr>
+                                        <ol>
+                                            <li>
+                                                <h4>Bagaimana Cara Berhenti Dari Layanan Ini?</h4>
+                                                <p>Cukup mengganti password instagram anda,dan otomatis anda berhenti dari layanan ini</p>
+                                            </li>
+                                            <li>
+                                                <h4>Kenapa Following Saya Ikut Bertambah?</h4>
+                                                <p>Karena layanan ini adalah tukar-menukar followers.Anda mendapatkan followers anda juga mendapatkan following.Anda dapat memention mereka untuk melakukan follback :)</p>
+                                            </li>
+                                            <li>
+                                                <h4>Saya Ingin Memberi Masukan terhadap layanan ini,kemana saya harus mengirimnya?</h4>
+                                                <p>Silahkan klik Menu 'Hubungi Kami' diatas. Terima Kasih - </p>
+                                            </li>
+                                        </ol><hr>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html> 
