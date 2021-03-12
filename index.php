bca01ea720c87f5c7c326e436a3a21446c48b916
belajarhtml14 <80535452+belajarhtml14@users.noreply.github.com>
2021-03-12 22:48:01 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Instagram Followers">
+    <meta name="author" content="MasterOgar RENDY">
+    <link href="http://gogram.net/assets/img/gogram.png" rel="icon" type="image/x-icon" />
+    <title>Instagram Followers</title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/main-style.css" rel="stylesheet">
+    <link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a href="Instagram Followers" onclick="this.href = 'index.php'" class="navbar-brand">Instagram Followers</a>        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="Dashboard" onclick="this.href = 'index.php'"><i class="glyphicon glyphicon-home"></i> Dashboard</a></li>
+            <li><a href="Peraturan" onclick="this.href = 'peraturan.php'"><i class="fa fa-info-circle"></i> Peraturan</a></li>
+            <li><a href="Hubungi kami" onclick="this.href = 'kontak.php'"><i class="glyphicon glyphicon-earphone"></i> Hubungi kami</a></li>
+            <li><a href="Masuk Instagram" onclick="this.href = 'masuk.php'"><i class="glyphicon glyphicon-off"></i> Masuk Instagram</a></li>          </ul>
+          </div>
+      </div>
+    </nav>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <img class="first-slide" src="assets/images/slide_01.jpg" alt="First slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Instagram Followers</h1>
+              <p>Situs penambah followers dan likes gratis yang telah digunakan oleh lebih dari <b>1 Juta</b> pengguna dari mancanegara</p>
+              <p><a onclick="this.href = 'masuk.php'" class="btn btn-info btn-outline" role="button">Masuk & Nikmati Layanan Kami</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img class="second-slide" src="assets/images/slide_02.jpg" alt="Second slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Instagram Followers</h1>
+              <p>Kami menyediakan layanan ini dengan gratis untuk anda tanpa dipungut biaya sepersen pun</p>
+              <p><a onclick="this.href = 'masuk.php'" class="btn btn-info btn-outline" role="button">Masuk & Nikmati Layanan Kami</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img class="third-slide" src="assets/images/slide_03.jpg" alt="Third slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Instagram Followers</h1>
+              <p>Situs kami bebas dari IKLAN yang membuat anda lebih nyaman saat menggunakannya</p>
+              <p><a onclick="this.href = 'masuk.php'" class="btn btn-info btn-outline" role="button">Masuk & Nikmati Layanan Kami</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+    <div class="container">
+      <div class="page-header text-center">
+        <h1>Hanya Untuk Anda</h1>
+      </div>
+      <p class="lead">Kami menyediakan layanan auto followers & likes ini <b>24 Jam</b> yang bisa anda gunakan dimana saja dan kapan saja,dan tentunya tanpa membuat spam</p>
+    </div>
+    <section>
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                        <i class="fa fa-shield fa-stack-1x fa-inverse"></i>
+                    </span>
+                    <h4 class="service-heading">Keamanan</h4>
+                    <p class="text-muted">Bicara mengenai keamanan,kami tidak pernah menyimpan/mengetahui kata sandi anda.Alasan kenapa kami memerlukan password anda pada saat masuk adalah agar data anda dapat terhubung ke <b>API Instagram</b> untuk dapat bertukar follow dan like.<b>Mengapa kami tidak menyimpan password?</b> karena kami tahu, bahwa <b>Privasi</b> adalah hal yang sangat penting bagi kita semua.Kami hanya ingin menjaga kepercayaan pengguna terhadap auto followers dan likes ini.</p>
+                </div>
+                <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                        <i class="fa fa-desktop fa-stack-1x fa-inverse"></i>
+                    </span>
+                    <h4 class="service-heading">Tampilan/Interface</h4>
+                    <p class="text-muted">Untuk urusan tampilan,jangan diragukan lagi karena situs kami sangat responsive dan cocok dibuka baik melalui <b>Smartphone,Tablet,Atau PC</b> anda.Situs kami juga <b>BEBAS</b> dari iklan,karena kami tahu bahwa iklan sangat mengganggu anda saat mengakses situs ini.Terlebih lagi jika anda menggunakan Smartphone/Tablet.</p>
+                </div>
+                <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                        <i class="fa fa-instagram fa-stack-1x fa-inverse"></i>
+                    </span>
+                    <h4 class="service-heading">Followers & Like</h4>
+                    <p class="text-muted">Kita semua tahu bahwa semakin banyak followers/likes yang kita punya dapat <b>meningkatkan popularitas,merk,dan juga status anda</b>.Hal ini tentunya akan sangat bermanfaat bagi anda yang berjualan online agar produk anda tidak kalah saing dengan produk dari toko online di instagram lainnya.Situs kami juga menggunakan sistem poin untuk bertukar like/follow yang setiap harinya di reset.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="container">
+    <hr>
+    </div>
+    <footer class="footer">
+      <div class="container">
+        <p class="text-muted"><i>&copy; 2017 Instagram Followers, Inc. All Right Reserved.</i></p>
+      </div>
+    </footer>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
